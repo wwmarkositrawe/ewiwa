4d0798edad42fd54220321a39fa673ee9f1ae8b3
wwmarkositrawe <wungielq@gmail.com>
2020-04-01 17:59:11 +0200

    somehow and somewhat is working but working  still in progress

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
 
 
     def publish(self):
